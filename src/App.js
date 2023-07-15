38af137bd679a1323fe5d218620c9b544ff0a804
santhiyasanthiya <santhiya30032@gmail.com>
2023-07-15 22:14:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, lazy } from 'react';
+import { Navigate, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { routes } from "./routes/routes";
+import SuspenseLoader from './components/Common/SuspenseLoader';
+import DataProvider from './context/DataProvider';
 
+const ErrorComponent = lazy(() => import('./components/Common/ErrorComponent'));
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path={routes.main.path} element={<Navigate to={`${routes.emails.path}/inbox`} />} />
+      <Route path={routes.main.path} element={<routes.main.element />} >
+      <Route path={`${routes.emails.path}/:type`} element={<routes.emails.element />} errorElement={<ErrorComponent />} />
+      <Route path={routes.view.path} element={<routes.view.element />} errorElement={<ErrorComponent />} />
+    </Route>
+
+      <Route path={routes.invalid.path} element={<Navigate to={`${routes.emails.path}/inbox`} />} />
+    </Route>
+  )
+)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<SuspenseLoader />}>
+      <DataProvider>
+        <RouterProvider router={router} />
+      </DataProvider>
+    </Suspense>
   );
 }
 
